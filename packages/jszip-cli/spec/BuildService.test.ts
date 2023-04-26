ce196bfcbdd6b2731d845ed82cb5e424b5653469
Florian Imdahl <git@ffflorian.de>
2023-04-26 20:36:03 +0200

    fix import

--- packages/jszip-cli/spec/BuildService.test.ts
+++ packages/jszip-cli/spec/BuildService.test.ts
@@ -1,7 +1,7 @@
 import fs from 'fs-extra';
 import {JSZipCLI} from '../src/index.js';
 import type {BuildService} from '../src/BuildService.js';
-import glob from 'glob';
+import {glob} from 'glob';
 
 describe('BuildService', () => {
   let jsZipCLI: JSZipCLI;
