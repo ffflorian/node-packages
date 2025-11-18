23d51c1ef413ec8198666c9071600621269e9f64
Florian Imdahl <git@ffflorian.de>
2025-11-18 13:41:39 +0100

    test(my-timezone): Simplify nock test

--- packages/my-timezone/src/MyTimezone.test.ts
+++ packages/my-timezone/src/MyTimezone.test.ts
@@ -10,63 +10,59 @@ vi.setConfig({
 const nominatimAPI = 'https://nominatim.openstreetmap.org';
 
 describe('MyTimezone', () => {
-  const tz = new MyTimezone({
+  const myTimezone = new MyTimezone({
     offline: true,
   });
-  let formatted_address: string;
 
   nock(nominatimAPI)
     .get('/search')
-    .query(queryObject => {
-      formatted_address = queryObject.q as string;
-      return true;
+    .query(true)
+    .reply(function () {
+      const parsedURL = new URL(this.req.path, 'http://example.com');
+      const formattedAddress = parsedURL.searchParams.get('q');
+
+      return [
+        200,
+        [
+          {
+            boundingbox: ['52.3570365', '52.6770365', '13.2288599', '13.5488599'],
+            class: 'place',
+            display_name: formattedAddress,
+            icon: 'https://nominatim.openstreetmap.org/images/mapicons/poi_place_city.p.20.png',
+            importance: 1.007539028249136,
+            lat: '52.5170365',
+            licence: 'Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright',
+            lon: '13.3888599',
+            osm_id: 240109189,
+            osm_type: 'node',
+            place_id: 595794,
+            type: 'city',
+          },
+        ],
+      ];
     })
-    .reply(() => [
-      200,
-      [
-        {
-          boundingbox: ['52.3570365', '52.6770365', '13.2288599', '13.5488599'],
-          class: 'place',
-          display_name: formatted_address,
-          icon: 'https://nominatim.openstreetmap.org/images/mapicons/poi_place_city.p.20.png',
-          importance: 1.007539028249136,
-          lat: '52.5170365',
-          licence: 'Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright',
-          lon: '13.3888599',
-          osm_id: 240109189,
-          osm_type: 'node',
-          place_id: 595794,
-          type: 'city',
-        },
-      ],
-    ])
     .persist();
 
   test('returns an address from Nominatim', async () => {
     const location = 'Berlin, Germany';
 
-    const {formattedAddress} = await tz.getLocationByName(location);
+    const {formattedAddress} = await myTimezone.getLocationByName(location);
 
     expect(formattedAddress).toBe(location);
   });
 
   test('returns the correct time for a location', async () => {
-    const berlinTime = await tz.getDateByLongitude(13.40803);
-    //console.log('Timezone at 52.51848, 13.40803:', berlinTime.toString());
-
-    const frankfurtTime = await tz.getDateByLongitude(8.67931);
-    //console.log('Timezone at 50.11796, 8.67931:', frankfurtTime.toString());
+    const berlinTime = await myTimezone.getDateByLongitude(13.40803);
+    const frankfurtTime = await myTimezone.getDateByLongitude(8.67931);
 
     expect(frankfurtTime.getTime() < berlinTime.getTime()).toBe(true);
   });
 
   test('returns the time for an address', async () => {
-    const dataBerlin = await tz.getDateByAddress('Berlin, Germany');
+    const dataBerlin = await myTimezone.getDateByAddress('Berlin, Germany');
     expect(dataBerlin).toBeDefined();
-    //console.log('Timezone Berlin:', dataBerlin.toString());
 
-    const dataMinsk = await tz.getDateByAddress('Minsk, Belarus');
+    const dataMinsk = await myTimezone.getDateByAddress('Minsk, Belarus');
     expect(dataMinsk).toBeDefined();
-    //console.log('Timezone Minsk:', dataMinsk.toString());
   });
 });
